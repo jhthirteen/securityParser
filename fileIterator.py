309066fe1b8638b6d0226e6386e3527665c1f733
Jack Hunter <jhunter5@binghamton.edu>
2024-09-26 14:17:16 -0400

    updated directory structure for how xml files are outputted

--- fileIterator.py
+++ fileIterator.py
@@ -5,8 +5,11 @@ import xml.etree.ElementTree as ElementTree
 
 def unzip_docx(file_path):
     docx = zipfile.ZipFile(file_path, 'r')
-    docx.extract('word/document.xml') #extract the xml file containing text contents 
-    shutil.move('./word/document.xml', '.') #move to home directory
+    docx.extract('word/document.xml') #extract the xml file containing text contents
+    end_location = file_path.find('.')
+    file_name = './text_docs/' + file_path[0:end_location] + '.xml'
+    shutil.move('./word/document.xml', './text_docs') #move to home directory
+    os.rename('./text_docs/document.xml', file_name)
     os.rmdir('./word') #delete the extracted directory
 
 def get_xml_root(file_path): #(./extracted_info/word/document.xml) path 
@@ -23,8 +26,14 @@ def get_text(xml_tree_root):
 
     return(text_formatted)
 
-def search_for_text(input_str, files):
-    pass
+def initParser():
+    os.mkdir('text_docs')
 
-unzip_docx('ir.docx')
-print(get_text(get_xml_root('document.xml')))
+def search_for_text(input_str, files):
+    for i in range(len(files)):
+        unzip_docx(files[i])
+        
+
+initParser()
+test_files = ['ir.docx']
+search_for_text('test', test_files)
