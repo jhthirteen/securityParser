72eebd664421a65905f4ef419b2bcb8e9cac0077
Jack Hunter <jhunter5@binghamton.edu>
2024-09-26 13:58:15 -0400

    updates

--- fileIterator.py
+++ fileIterator.py
@@ -1,9 +1,13 @@
 import zipfile
+import shutil
+import os
 import xml.etree.ElementTree as ElementTree
 
 def unzip_docx(file_path):
-    docx = zipfile.ZipFile(file_path, 'r') #create a zipfile object out of the .docx
-    docx.extractall('extracted_info') #unzips -> moves files into extracted_info directory
+    docx = zipfile.ZipFile(file_path, 'r')
+    docx.extract('word/document.xml') #extract the xml file containing text contents 
+    shutil.move('./word/document.xml', '.') #move to home directory
+    os.rmdir('./word') #delete the extracted directory
 
 def get_xml_root(file_path): #(./extracted_info/word/document.xml) path 
     xml_tree = ElementTree.parse(file_path) #creates an parse tree out out of the xml file passed in 
@@ -19,5 +23,8 @@ def get_text(xml_tree_root):
 
     return(text_formatted)
 
+def search_for_text(input_str, files):
+    pass
+
 unzip_docx('ir.docx')
-get_text(get_xml_root('./extracted_info/word/document.xml'))
+print(get_text(get_xml_root('document.xml')))
